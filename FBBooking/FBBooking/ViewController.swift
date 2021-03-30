8f072a51814e87f538e1ea9132b630ede4d94761
Tran Thai Tan <tantan992@gmail.com>
2021-03-30 11:35:46 -0400

    init project

---
+++ FBBooking/FBBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBBooking
+//
+//  Created by Tan Tan on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
